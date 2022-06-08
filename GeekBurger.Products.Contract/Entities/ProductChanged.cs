87e0c57ae81cc4bbcdaba58df8d4edfbf8ab0c8a
ALDO CESAR VISCOVICCE <RM342846@fiap.com.br>
2022-06-08 00:20:12 -0300

    Add project files.

---
+++ GeekBurger.Products.Contract/Entities/ProductChanged.cs
@@ -0,0 +1,11 @@
+﻿using GeekBurger.Products.Contract.Enum;
+
+namespace GeekBurger.Products.Contract.Entities
+{
+    public class ProductChanged
+    {
+        public ProductState State { get; set; }
+        public Product Product { get; set; }
+    }
+
+}
